60160c0875a57d5f4ad59f9676f86e3b0747e87a
jdorkin11 <jdorkin@purdue.edu>
2021-05-02 16:27:03 -0400

    Update LoginGUI.java
    Created panel upon click of "Sign Up" button made for creation of new account. Login button still just prints username and password, new createAccount button prints confirmation of account creation but doesn't actually do anything with it

--- src/LoginGUI.java
+++ src/LoginGUI.java
@@ -1,4 +1,3 @@
-package src;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -10,9 +9,11 @@ public class LoginGUI implements ActionListener
     private static JPasswordField passwordText;
     private static JButton loginButton;
     private static JButton signUpButton;
+    private static JButton createAccountButton;
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
+        JPanel signUpPanel = new JPanel();
         frame.setSize(350,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
@@ -41,6 +42,7 @@ public class LoginGUI implements ActionListener
         
         signUpButton = new JButton("Sign Up");
         signUpButton.setBounds(180,95,80,25);
+        signUpButton.addActionListener(new LoginGUI());
         panel.add(signUpButton);
         
         frame.setVisible(true);
@@ -48,8 +50,43 @@ public class LoginGUI implements ActionListener
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        String username = userText.getText();
-        String password = passwordText.getText();
-        System.out.println(username + ", " + password);
+        if(e.getSource() == loginButton) {
+            String username = userText.getText();
+            String password = passwordText.getText();
+            System.out.println(username + ", " + password);
+        }
+        else if(e.getSource() == signUpButton) {
+            JFrame frame = new JFrame();
+            JPanel panel = new JPanel();
+            frame.setSize(350,200);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(panel);
+            panel.setLayout(null);
+            
+            usernameLabel = new JLabel("Username");
+            usernameLabel.setBounds(10,20,80,25);
+            panel.add(usernameLabel);
+        
+            userText = new JTextField();
+            userText.setBounds(100,20,165,25);
+            panel.add(userText);
+        
+            passwordLabel = new JLabel("Password");
+            passwordLabel.setBounds(10,50,80,25);
+            panel.add(passwordLabel);
+        
+            passwordText = new JPasswordField();
+            passwordText.setBounds(100,50,165,25);
+            panel.add(passwordText);
+            
+            createAccountButton = new JButton("Create Account");
+            createAccountButton.setBounds(100,95,140,25);
+            createAccountButton.addActionListener(new LoginGUI());
+            panel.add(createAccountButton);
+            
+            frame.setVisible(true);
+        } else if(e.getSource() == createAccountButton) {
+            System.out.println("Account created with username " + userText.getText() + " and password " + passwordText.getText());
+        }
     }
 }
